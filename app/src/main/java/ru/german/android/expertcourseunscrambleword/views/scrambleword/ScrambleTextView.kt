4d72facdf64b1d7cd138e2b043fbb3bd6a81acc6
GermanFromSP <153195318+GermanFromSP@users.noreply.github.com>
2024-09-02 11:05:28 +0300

    UG-14 create custom view and XML

--- app/src/main/java/ru/german/android/expertcourseunscrambleword/views/scrambleword/ScrambleTextView.kt
+++ app/src/main/java/ru/german/android/expertcourseunscrambleword/views/scrambleword/ScrambleTextView.kt
@@ -1,18 +1,18 @@
 package ru.german.android.expertcourseunscrambleword.views.scrambleword
 
-    import android.content.Context
-    import android.util.AttributeSet
-    import androidx.appcompat.widget.AppCompatTextView
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
 
 class ScrambleTextView : AppCompatTextView, UpdateText {
-        
-        constructor(context: Context) : super(context)
-        constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
-        constructor(context: Context, attributeSet: AttributeSet, defStyleAttr: Int) : super(
-            context,
-            attributeSet,
-            defStyleAttr
-        )
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
+    constructor(context: Context, attributeSet: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attributeSet,
+        defStyleAttr
+    )
 
     override fun getFreezesText(): Boolean = true
 
